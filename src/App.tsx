3a1cad00a9621b17ded09a0ce5e0ddd4574df4c5
Jorge Assaf <jorgeassaf160@gmail.com>
2023-05-30 22:02:03 -0600

    refactor: 💡 layout global

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home, Cart, ProductItem } from './pages'
+import Navbar from './components/Navbar'
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/products' element={<Home />} />
-      <Route path='/cart' element={<Cart />} />
-      <Route path='/:title' element={<ProductItem />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/:id' element={<ProductItem />} />
+      </Routes>
+    </>
   )
 }
 
