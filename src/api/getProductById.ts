5ce72b76218a3dc72827fb3f90805872690f507f
Jorge Assaf <jorgeassaf160@gmail.com>
2023-05-30 22:03:06 -0600

    feat: 🎸 add getProductsById

---
+++ src/api/getProductById.ts
@@ -0,0 +1,8 @@
+import { Products } from "../types"
+
+
+export const getProductById = async (id: string): Promise<Products> => {
+  const response = await fetch(`https://fakestoreapi.com/products/${id}`)
+  const product = await response.json()
+  return product
+}
--- src/pages/ProductItem.tsx
+++ src/pages/ProductItem.tsx
@@ -1,48 +1,96 @@
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { Products } from '../types'
-import { useEffect, useState } from 'react'
+import { useQuery } from 'react-query'
+import { Minus, Plus, ShoppingCart } from 'lucide-react'
+import { useCartProduct, useQuatity } from '../Store/Product'
+import { getProductById } from '../api/getProductById'
+
+import type { CartProduct, Products } from '../types'
 
 const ProductItem = () => {
-  const params = useParams()
-  const [product, setProduct] = useState({} as Products)
+  const { id } = useParams()
 
-  useEffect(() => {
-    const fetchProducts = async (): Promise<Products> => {
-      const response = await fetch('https://fakestoreapi.com/products')
-      const products = await response.json()
+  const { quantity, setQuantity } = useQuatity((state) => state)
+  const { addToCart, cart } = useCartProduct((state) => state)
 
-      const product = products.find(
-        (product: Products) => product.title == params.title,
-      )
+  const {
+    data: product,
+    isLoading,
+    isFetching,
+    error,
+  } = useQuery('product', () => getProductById(`${id}`))
 
-      console.log(product)
-      setProduct(product)
-      return products
+  const handleCart = (product: Products) => {
+    const cartProduct: CartProduct = {
+      ...product,
+      quantity: quantity,
     }
-    fetchProducts()
-  }, [params.title])
-
+    addToCart(cartProduct)
+  }
+  if (error) {
+    return <div>Something went wrong...</div>
+  }
   return (
-    <div>
-      <div className='mt-4 px-5 pb-5'>
+    <div className='mt-10 w-90% m-auto '>
+      {isFetching ? (
+        <div>Loading...</div>
+      ) : (
+        <div >
 
-        <h5 className='text-xl tracking-tight text-slate-900'>
-          {product?.title}
-        </h5>
-        <img src={product.image} alt="" />
+          <img
+            className='object-cc object-contain mx-auto h-md '
+            src={product?.image}
+            alt={product?.title}
+          />
 
-        <div className='mt-2 mb-5 flex items-center justify-between'>
-          <p>
-            <span className='text-3xl font-bold text-slate-900'>
-              ${product?.price}
-            </span>
-            {/* <span className='text-sm text-slate-900 line-through'>
+          <div className='max-w-2xl m-auto '>
+            <h5 className='text-3xl  text-slate-900'>{product?.title}</h5>
+            <div className='mt-2 mb-5 flex items-center justify-between'>
+
+              <p>
+                <span className='text-3xl font-bold text-slate-900'>
+                  {product?.price?.toLocaleString('en-US', {
+                    style: 'currency',
+                    currency: 'USD',
+                  })}
+                </span>
+                {/* <span className='text-sm text-slate-900 line-through'>
                       $699
                     </span> */}
-          </p>
+              </p>
+            </div>
+            <p className='my-5'>{product?.description}</p>
 
+            <div className='flex gap-6 items-center'>
+              <button
+                className='bg-slate-900 py-1 px-3 rounded-lg text-white text-xl'
+                onClick={() => setQuantity(quantity - 1)}
+              >
+                <Minus />
+              </button>
+              <p>{quantity}</p>
+              <button
+                className='bg-slate-900 py-1 px-3 rounded-lg text-white text-xl'
+                onClick={() => setQuantity(quantity + 1)}
+              >
+                <Plus />
+              </button>
+            </div>
+            <button className='btn mt-5 mb-10 items-center' onClick={() => handleCart(product!)}>
+              <ShoppingCart
+                size={20}
+                className='inline-block  mr-2'
+                strokeWidth={2}
+              />
+              {cart.find(
+                (cartProduct: CartProduct) => cartProduct.id == product?.id,
+              )
+                ? 'Added to Cart'
+                : 'Add to Cart'}
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
