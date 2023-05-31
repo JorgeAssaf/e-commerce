cf897f3558398a76416295ae9dee99013b40059e
Jorge Assaf <jorgeassaf160@gmail.com>
2023-05-30 22:01:17 -0600

    feat: 🎸 add deasy ui and lucide icons

--- uno.config.ts
+++ uno.config.ts
@@ -2,11 +2,18 @@
 
 import { defineConfig } from 'unocss'
 import presetUno from '@unocss/preset-uno'
+import { presetDaisy } from 'unocss-preset-daisy'
 import presetWebFonts from '@unocss/preset-web-fonts'
 
 export default defineConfig({
   presets: [
     presetUno(),
+    presetDaisy({
+
+
+      themes: ['light'],
+
+    }),
     presetWebFonts({
       // use axios with an https proxy
       provider: 'fontshare',
