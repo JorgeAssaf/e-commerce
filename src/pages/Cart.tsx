a3f106305a2d6e1bbcd518dcba146e0062f86e77
Jorge Assaf <jorgeassaf160@gmail.com>
2023-05-30 22:05:00 -0600

    feat: 🎸 add total and subtotal

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,24 +1,28 @@
 import { useAuth0 } from '@auth0/auth0-react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useCartProduct } from '../Store/Product'
-import { Products } from '../types'
+import type { CartProduct } from '../types'
 
 const Cart = () => {
   const navigate = useNavigate()
   const { isAuthenticated, isLoading } = useAuth0()
   const { cart } = useCartProduct((state) => state)
-
+  const { removeFromCart } = useCartProduct((state) => state)
   useEffect(() => {
     if (!isAuthenticated && !isLoading) {
       navigate('/')
     }
     return
   }, [isAuthenticated, navigate, isLoading])
+  console.log(cart);
 
+  const total = cart.reduce((acc, curr) => {
+    return acc + curr.price * curr.quantity
+  }, 0)
   return (
-    <div>
-      {cart.map((product: Products) => {
+    <div className='w-90% m-auto '>
+      {cart.map((product: CartProduct) => {
         return (
           <div key={product.id}>
             <h1>{product.title}</h1>
@@ -32,9 +36,14 @@ const Cart = () => {
             <p>
               {product.quantity}
             </p>
+            <p>
+              Total : {product.price * product.quantity}
+            </p>
+            <button className='btn' onClick={() => removeFromCart(product.id)}>Remove</button>
           </div>
         )
       })}
+      <h1>Total : {total}</h1>
     </div>
   )
 }
