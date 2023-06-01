75dd7522628bd63661361825da596ad851af8602
Jorge Assaf <jorgeassaf160@gmail.com>
2023-06-01 15:50:43 -0600

    prueva

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Auth0Provider } from '@auth0/auth0-react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App.tsx'
 import 'virtual:uno.css'
 import '@unocss/reset/tailwind-compat.css'
+import { Auth0ProviderWithNavigate } from './auth0-provider-with-navigate.js.tsx'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0Provider
-        domain='dev-a0nqbtp3nfvu0u0b.us.auth0.com'
-        clientId='2AjFDw2uJ7pY2yu4YiETdaInm4QJo6MO'
-      >
+      <QueryClientProvider client={queryClient}>
 
-        <QueryClientProvider client={queryClient}>
-          <App />
-        </QueryClientProvider>
+        <App />
 
-      </Auth0Provider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
