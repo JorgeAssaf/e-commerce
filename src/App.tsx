a63ce8add8335785a64ee937a7d83929a5a5b8aa
Jorge Assaf <jorgeassaf160@gmail.com>
2023-06-01 12:49:15 -0600

    fix: 🐛 code

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom'
 
 import { Home, Cart, ProductItem } from './pages'
 import Navbar from './components/Navbar'
+import { ArrowLeft } from 'lucide-react'
+import Products from './pages/Products'
 
 function App() {
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/products' element={<Home />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/:id' element={<ProductItem />} />
-      </Routes>
+
+      <div className='w-90% mx-auto mt-5 '>
+        <span
+          className={`${pathname === '/' ? 'hidden' : 'flex'} mb-5`}
+          onClick={() => navigate(-1)}
+        >
+          <ArrowLeft size='35' />
+        </span>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/:id' element={<ProductItem />} />
+        </Routes>
+      </div>
     </>
   )
 }
--- src/api/fetchProducts.ts
+++ src/api/fetchProducts.ts
@@ -1,7 +1,11 @@
 import { Products } from '../types'
 
-export const fetchProducts = async (query: string): Promise<Products[]> => {
-  const response = await fetch(`https://fakestoreapi.com/products`)
+export const fetchProducts = async (
+  limit = '20',
+): Promise<Products[]> => {
+  const response = await fetch(
+    `https://fakestoreapi.com/products?limit=${limit}`,
+  )
   const products = await response.json()
   return products
 }
