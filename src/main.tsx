7ff50f0b24024e41924ce9cdd8820920a62035fa
Jorge Assaf <jorgeassaf160@gmail.com>
2023-06-01 15:55:16 -0600

    Revert "prueva"
    This reverts commit 75dd7522628bd63661361825da596ad851af8602.

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App.tsx'
 import 'virtual:uno.css'
 import '@unocss/reset/tailwind-compat.css'
-import { Auth0ProviderWithNavigate } from './auth0-provider-with-navigate.js.tsx'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
+      <Auth0Provider
+        domain='dev-a0nqbtp3nfvu0u0b.us.auth0.com'
+        clientId='2AjFDw2uJ7pY2yu4YiETdaInm4QJo6MO'
+      >
 
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
 
-      </QueryClientProvider>
+      </Auth0Provider>
     </BrowserRouter>
   </React.StrictMode>,
 )
