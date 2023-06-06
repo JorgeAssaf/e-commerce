7a5ac2e525b03fc0df54a53b447246789b8671f4
Jorge Assaf <jorgeassaf160@gmail.com>
2023-06-05 21:28:56 -0600

    feat: 🎸 add million and swiper

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
-
 import react from '@vitejs/plugin-react-swc'
+import million from 'million/compiler';
+
 import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
